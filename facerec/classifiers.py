2113cfc4957847405c492b8c4d17bb38d9f7e59e
bkneis <b.kneis2011@gmail.com>
2018-01-27 17:10:59 +0000

    Rename classifiers file

--- facerec/classifiers.py
+++ facerec/classifiers.py
@@ -13,11 +13,12 @@ class StatModel(object):
 
 class SVM(StatModel):
     """wrapper for OpenCV SimpleVectorMachine algorithm"""
-    def __init__(self):
+    def __init__(self, C=1, gamma=0.5):
         self.model = cv2.ml.SVM_create()
         self.model.setType(cv2.ml.SVM_C_SVC)
-        self.model.setKernel(cv2.ml.SVM_LINEAR)
-        self.model.setC(1)
+        self.model.setKernel(cv2.ml.SVM_RBF)
+        self.model.setC(C)
+        self.model.setGamma(gamma)
         # svm.setDegree(0.0)
         # svm.setGamma(0.0)
         # svm.setCoef0(0.0)
@@ -29,4 +30,17 @@ class SVM(StatModel):
         self.model.train(samples, cv2.ml.ROW_SAMPLE, responses)
 
     def predict(self, samples):
-        return self.model.predict(samples)
+        return self.model.predict(samples)[1].ravel()
+
+
+class KNearest(StatModel):
+    def __init__(self, k=1):
+        self.k = k
+        self.model = cv2.ml.KNearest_create()
+
+    def train(self, samples, responses):
+        self.model.train(samples, cv2.ml.ROW_SAMPLE, responses)
+
+    def predict(self, samples):
+        _retval, results, _neigh_resp, _dists = self.model.findNearest(samples, self.k)
+        print('knn', _retval, results, _neigh_resp, _dists)
