417b3e337ab9eb9ee23120d9274ee3ba32621928
bkneis <b.kneis2011@gmail.com>
2018-01-27 15:14:10 +0000

    Add cv2 flag when reading file

--- facerec/main.py
+++ facerec/main.py
@@ -5,7 +5,7 @@ from FaceDetector import FaceDetector
 
 
 def main():
-    img = cv2.imread('/home/arthur/face.png')
+    img = cv2.imread('/home/arthur/face.png', 0)
 
     lbp = LBP()
     detector = FaceDetector()
@@ -13,9 +13,8 @@ def main():
     face = detector.detect(img)
     if face is not None:
         face = detector.crop_face(img, face)
-        cv2.imwrite('/home/arthur/test.png', face)
-        # hist, bins = lbp.run(face, True)
-        # print(hist, bins)
+        hist, bins = lbp.run(face, False)
+        print(hist, bins)
 
 
 if __name__ == '__main__':
