00d8a9666c9d81c5b70965ddb7213109f536131a
bkneis <b.kneis2011@gmail.com>
2018-01-31 16:15:27 +0000

    Add file for config values

---
+++ facerec/config.py
@@ -0,0 +1,6 @@
+# Configs for application
+
+THRESHOLD = 200000  # Threshold for the classifier
+SUBJECTS_FOLDER = "/home/arthur/subjects"  # Folder where the subjects are loaded
+DEBUG = False  # If images and performances should be logged
+LBP_TYPE = "BASIC"  # BASIC or UNIFORM
