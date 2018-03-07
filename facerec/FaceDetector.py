79f7ad92ba8366ce505b0458aacf213e87afe63b
bkneis <b.kneis2011@gmail.com>
2018-03-07 16:13:33 +0000

    Increase min size for face detector

--- facerec/FaceDetector.py
+++ facerec/FaceDetector.py
@@ -24,7 +24,7 @@ class FaceDetector:
             raise ImageNotProvidedException
 
         try:
-            faces = self.face_cascade.detectMultiScale(img, 1.05, 3, minSize=(30, 30))
+            faces = self.face_cascade.detectMultiScale(img, 1.05, 3, minSize=(200, 200))
         except cv2.error as err:
             print('Detect multi scale failed', err)
             faces = []
